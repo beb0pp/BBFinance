670becc05bc2e3729c1187238222d0922e93355a
Luis Felipe Macário de Abreu <labreu@ativainvestimentos.com.br>
2023-04-04 17:06:43 -0300

    mudando pastas

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='BBFinance',
+    version='0.1.0',
+    description='Uma biblioteca com o objetivo de adquirir informaçõe de ações do mercado financeiro de maneira rapida e pratica, afim de incluir todos no mercado',
+    url='https://github.com/beb0pp/BBFinance',
+    author='Luis Abreu',
+    author_email='luss.fel@gmail.com',
+    license='MIT',
+    packages=['main'],
+    install_requires=[
+        'yfinance>=0.2.12',
+        'scipy>=1.9.3',
+        'selenium>=3.141.0',
+        'pandas-datareader>=0.10.0',
+        'json5>=0.9.6',
+        'numpy>=1.23.4',
+        'uvicorn>=0.21.1',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
