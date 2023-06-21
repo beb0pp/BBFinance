a9309dd6998033861a1c5c0ad26823e916a70724
beb0pp <luss.fel@gmail.com>
2023-06-20 22:24:01 -0300

    :heavy_minus_sign: build: Fazendo a remoçao de algumas dependencias

--- build/lib/BBFinance/__init__.py
+++
@@ -1,5 +0,0 @@
-from .BBFinance import *
-
-__all__ = ['get_beta', 'get_info', 'get_stock_history', 'get_stock_technicals', 
-           'get_stock_trend', 'get_volatility', 'get_var', 'asset_portfolio', 'markowitz_allocation',
-           'get_funds', 'compare_setor_funds', 'compare_funds', 'best_assets', 'best_assets_value']
