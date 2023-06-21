87d886c328d56fddbf069e4d0d30ba673282bcaa
beb0pp <luss.fel@gmail.com>
2023-06-20 22:37:16 -0300

    :recycle: refactor: Retirando funcionalidades e recursos desnecessarios

--- setup.py
+++ setup.py
@@ -26,10 +26,8 @@ setup(
         'requests>=2.28.2',
         'bs4>=0.0.1',
         'beautifulsoup4>=4.11.2',
-        'pydantic>=1.10.7',
         'Unicode>=1.3.6',
         'typing_extensions>=4.5.0',
-        'scikit-image>=0.19.3',
         'scikit-learn>=1.2.2',
         'tensorflow==2.12.0'
         
